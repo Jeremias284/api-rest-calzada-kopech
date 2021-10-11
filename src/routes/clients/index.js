13c18c1857937e0892a58fa081bcc3a4f1f30929
ivan kopech <ivankopech@gmail.com>
2021-10-11 17:16:33 -0300

    API REST: ruta de obtener cliente por ID. Comentarios indicando que hace cada una de las funciones

--- src/routes/clients/index.js
+++ src/routes/clients/index.js
@@ -3,8 +3,14 @@ const controller = require('../../controllers/clients') // controller es control
 
 const router = express.Router();
 
+//OBTIENE TODOS LOS CLIENTES
 router.route('/').get(controller.getClients); 
+//OBTIENE EL CLIENTE CON EL ID INDICADO
+router.route('/').get(controller.getClientById);
+
+//AGREGA CLIENTE
 router.route('/').post(controller.addClient); 
+//ELIMINA CLIENTE
 router.route('/:clientId').delete(controller.deleteClientById); 
 
 module.exports = router;
